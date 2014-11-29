ea69229971d41460741e445251210be503263008
uranos <uranoskoro@gmail.com>
2014-11-30 04:50:58 +0900

    update ADK

--- AgentDevelopmentKit/src/main/java/adk/sample/basic/tactics/BasicPolice.java
+++ AgentDevelopmentKit/src/main/java/adk/sample/basic/tactics/BasicPolice.java
@@ -12,6 +12,7 @@ import comlib.manager.MessageManager;
 import comlib.message.information.BuildingMessage;
 import comlib.message.information.CivilianMessage;
 import rescuecore2.config.Config;
+import rescuecore2.misc.Pair;
 import rescuecore2.misc.geometry.GeometryTools2D;
 import rescuecore2.misc.geometry.Line2D;
 import rescuecore2.misc.geometry.Point2D;
@@ -20,7 +21,10 @@ import rescuecore2.standard.entities.*;
 import rescuecore2.worldmodel.ChangeSet;
 import rescuecore2.worldmodel.EntityID;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public abstract class BasicPolice extends TacticsPolice implements RouteSearcherProvider, BlockadeSelectorProvider {
 
@@ -32,6 +36,11 @@ public abstract class BasicPolice extends TacticsPolice implements RouteSearcher
 	public void preparation(Config config) {
 		this.routeSearcher = this.initRouteSearcher();
 		this.blockadeSelector = this.initBlockadeSelector();
+        //
+        this.passableEdgeMap = new HashMap<>();
+        this.isClearTime = false;
+        this.targetRoad = null;
+        this.targetPoints = null;
 	}
 
     public abstract BlockadeSelector initBlockadeSelector();
@@ -40,6 +49,7 @@ public abstract class BasicPolice extends TacticsPolice implements RouteSearcher
 
     @Override
     public Action think(int currentTime, ChangeSet updateWorldData, MessageManager manager) {
+        this.agentPosition = new Point2D(this.me().getX(), this.me().getY());
         this.updateInfo(updateWorldData, manager);
         // if(this.util == null) {
         this.target = this.blockadeSelector.getTarget(currentTime);
@@ -62,7 +72,9 @@ public abstract class BasicPolice extends TacticsPolice implements RouteSearcher
 			}
 
 			if(bestPoint != null) {
+                //回転とか求める必要なし．ただの2点の差
 				Vector2D v = bestPoint.minus(new Point2D(this.me.getX(), this.me.getY()));
+                //ここで対象との差を変更して，一定の長さの距離にしている
 				v = v.normalised().scale(1000000);
 				this.blockadeSelector.remove(blockade);
 				return new ActionClear(this, currentTime, blockade, (int) (this.me.getX() + v.getX()), (int) (this.me.getY() + v.getY()));
@@ -110,4 +122,66 @@ public abstract class BasicPolice extends TacticsPolice implements RouteSearcher
     public RouteSearcher getRouteSearcher() {
         return this.routeSearcher;
     }
+
+    public Map<EntityID, List<Edge>> passableEdgeMap;
+
+    public List<Edge> getPassableEdge(Blockade blockade) {
+        EntityID roadID = blockade.getPosition();
+        List<Edge> edges = this.passableEdgeMap.get(roadID);
+        return edges != null ? edges : this.getPassableEdge((Road) this.getWorld().getEntity(roadID));
+    }
+
+    public List<Edge> getPassableEdge(Road road) {
+        EntityID roadID = road.getID();
+        List<Edge> edges = this.passableEdgeMap.get(roadID);
+        if(edges == null) {
+            edges = new ArrayList<>();
+            for(Edge edge : road.getEdges()) {
+                if(edge.isPassable()) {
+                    edges.add(edge);
+                }
+            }
+            this.passableEdgeMap.put(roadID, edges);
+        }
+        return edges;
+    }
+
+    public boolean isClearTime;
+
+    public Point2D agentPosition;
+
+    public Road targetRoad;
+    public List<Point2D> targetPoints;
+
+    public List<Point2D> getClearPointArray(Blockade blockade) {
+        return this.getClearPointArray(this.getPassableEdge(blockade));
+    }
+
+    public List<Point2D> getClearPointArray(Road road) {
+        return this.getClearPointArray(this.getPassableEdge(road));
+    }
+
+    public List<Point2D> getClearPointArray(List<Edge> edges) {
+        int size = edges.size();
+        List<Point2D> points = new ArrayList<>();
+        for (Edge edge : edges) {
+            points.add(new Point2D(((edge.getStartX() + edge.getEndX()) / 2), ((edge.getStartY() + edge.getEndY()) / 2)));
+        }
+        return points;
+    }
+
+    public Point2D getTargetPosition(AmbulanceTeam ambulance, Road road) {
+        EntityID roadID = road.getID();
+        if(!roadID.equals(ambulance.getPosition())) {
+            return null;
+        }
+        List<Edge> edges = this.getPassableEdge(road);
+        if(edges.isEmpty()) {
+            return null;
+        }
+
+        return null;
+    }
+
+
 }
