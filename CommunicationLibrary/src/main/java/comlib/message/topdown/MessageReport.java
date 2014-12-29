a378deb539b9365f2b601f955030649d3e61f2bb
Shunki TAKAMI <tkmnet@users.noreply.github.com>
2014-12-29 19:57:36 +0900

    update Comlib

--- CommunicationLibrary/src/main/java/comlib/message/topdown/MessageReport.java
+++ CommunicationLibrary/src/main/java/comlib/message/topdown/MessageReport.java
@@ -2,9 +2,12 @@ package comlib.message.topdown;
 
 import comlib.message.CommunicationMessage;
 import comlib.message.MessageID;
+import rescuecore2.worldmodel.EntityID;
 
 public class MessageReport extends CommunicationMessage
 {
+	private int rawFromID;
+	private EntityID reportFromID;
 	private boolean reportDone;
 
 	public MessageReport(boolean isDone)
@@ -17,6 +20,13 @@ public class MessageReport extends CommunicationMessage
 	{
 		super(MessageID.reportMessage, time, ttl);
 		reportDone = isDone;
+	}
+
+	public EntityID getFromID()
+	{
+		if ( reportFromID == null )
+		{ reportFromID = new EntityID(rawFromID); }
+		return reportFromID;
 	}
 
 	public boolean isDone()
