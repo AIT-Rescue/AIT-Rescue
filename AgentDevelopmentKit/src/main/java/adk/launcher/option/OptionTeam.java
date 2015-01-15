d5bb814a59c9d61e1c2e1c11e3f9811fb31faad8
Shunki TAKAMI(takamin@sylph.maslab) <maslab@takamin.net>
2015-01-16 05:43:25 +0900

    update ADK

--- AgentDevelopmentKit/src/main/java/adk/launcher/option/OptionTeam.java
+++ AgentDevelopmentKit/src/main/java/adk/launcher/option/OptionTeam.java
@@ -13,12 +13,21 @@ public class OptionTeam extends Option {
     @Override
     public void setValue(Config config, String[] datas) {
         if(datas.length == 5) {
-            config.setValue(ConfigKey.KEY_AMBULANCE_AGENT_NAME, datas[1]);
-            config.setValue(ConfigKey.KEY_AMBULANCE_AGENT_COUNT, datas[2]);
             config.setValue(ConfigKey.KEY_FIRE_AGENT_NAME, datas[1]);
-            config.setValue(ConfigKey.KEY_FIRE_AGENT_COUNT, datas[3]);
+            config.setValue(ConfigKey.KEY_FIRE_STATION_NAME, datas[1]);
             config.setValue(ConfigKey.KEY_POLICE_AGENT_NAME, datas[1]);
+            config.setValue(ConfigKey.KEY_POLICE_STATION_NAME, datas[1]);
+            config.setValue(ConfigKey.KEY_AMBULANCE_AGENT_NAME, datas[1]);
+            config.setValue(ConfigKey.KEY_AMBULANCE_STATION_NAME, datas[1]);
+
+            config.setValue(ConfigKey.KEY_FIRE_AGENT_COUNT, datas[2]);
+            config.setValue(ConfigKey.KEY_FIRE_STATION_COUNT, datas[3]);
+
             config.setValue(ConfigKey.KEY_POLICE_AGENT_COUNT, datas[4]);
+            config.setValue(ConfigKey.KEY_POLICE_STATION_COUNT, datas[5]);
+
+            config.setValue(ConfigKey.KEY_AMBULANCE_AGENT_COUNT, datas[6]);
+            config.setValue(ConfigKey.KEY_AMBULANCE_STATION_COUNT, datas[7]);
         }
     }
 }
